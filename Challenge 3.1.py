7f2c8f31b33dfbd420ed362f572e6f33a5614981
Sandy-1438ywusux <144475612+Sandy-1438ywusux@users.noreply.github.com>
2023-09-29 16:47:50 +0530

    Add files via upload

---
+++ Challenge 3.1.py
@@ -0,0 +1,24 @@
+def sort_students(student_list):
+    sorted_students = sorted(student_list, key=lambda x: x.cgpa, reverse=True)
+    return sorted_students
+
+
+class Student:
+    def __init__(self, name, roll_number, cgpa):
+        self.name = name
+        self.roll_number = roll_number
+        self.cgpa = cgpa
+
+
+students = [
+    Student("aradhya", "A101", 8.3),
+    Student("maghizhini", "B102", 9.5),
+    Student("saraa", "C103", 5.6),
+    Student("sandhiya","D104",10.0)
+]
+
+sorted_students = sort_students(students)
+
+
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}")
